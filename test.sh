fbbe92bf347538559dcd09a6733899d56ba6ffe0
Luyao Zhong <luyao.zhong@intel.com>
2019-04-16 03:36:13 +0000

    test more times for each scenario
    test 3 times for each benchmark，then calculate the average num

--- test.sh
+++ test.sh
@@ -3,5 +3,12 @@
 for i in {1..12}
 do
 	export INST_NUM=$i
-	./redis_benchmark.sh
+	export TEST_NUM=3
+	a=0
+	for((j=1;j<=$TEST_NUM;j++))
+	do
+		b=`./redis_benchmark.sh`
+		a=`echo "$a + $b" | bc`
+	done
+	echo "$a/$TEST_NUM" | bc
 done
